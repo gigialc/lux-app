ab44403faeda9fb5176f921f64264fc1588fa9bd
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-08-22 09:41:15 -0400

    hi

--- lux-landing-page/app/page.js
+++ lux-landing-page/app/page.js
@@ -7,7 +7,7 @@ const LuxLandingPage = () => {
       <header className="bg-[url('/couple.jpeg')] bg-cover bg-center flex-grow flex items-center w-full">
         <div className="w-full text-center text-cream px-4 py-12 md:py-20 lg:py-32">
           <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-white">Find Your Perfect Match</h2>
-          <p className="text-lg md:text-xl lg:text-2xl mb-8">Exclusive dating for international students coming soon...</p>
+          <p className="text-white md:text-xl lg:text-2xl mb-8">Exclusive dating for international students coming soon...</p>
           <a href="https://forms.gle/84aRztRnR576FARf7" target="_blank" rel="noopener noreferrer">
             <button className="bg-burgundy text-cream px-6 py-3 md:px-8 md:py-4 rounded-full text-lg md:text-xl font-semibold hover:bg-burgundy-light transition-colors">
               Apply Now
